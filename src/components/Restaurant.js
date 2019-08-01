90b9e62a2b3b12e44933c0b351fc5ee6b410f4b9
cdrishinski <cdrishinski@gmail.com>
2019-08-01 08:37:29 -0600

    trying to parse further through json

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react'
 import RestaurantList from '../RestaurantList.json';
 
+
 export default class Restaurant extends Component {
-    // constructor(props){
-    //     super(props)
-    // }
+    constructor(props){
+        super(props)
+        this.state= {
+          restDetail: 
+              {
+                  name: '',
+                  times: ''
+              }
+    }
+}
+
+
 
    
 
     render() {
-        // var Data = require('json!../restaurants.json');
         return (
             <div>
                 <h1>Restaurant page!</h1>
-                {RestaurantList.map((restDetail, index) =>{
-                    return (
-                        <div>
-                            <h1>{restDetail.name}</h1>
-                            <p>{restDetail.times[0]}</p>
-                            <p>{restDetail.times[1]}</p>
-                        </div>
+            
+                { RestaurantList.map( (restDetail, index) =>{
+                    return(
+                        <ul>
+                            <li>{ restDetail.name }
+                            </li>
+                                <ul>
+                                    <li> { restDetail.times[0] }</li>
+                                    <li> { restDetail.times[1] }</li>
+                                    <li> { restDetail.times[2] }</li>
+                                </ul>
+                        </ul>
+                        
                     )
+                    
                 })}
+               
+
              </div>
-        )
+            )
+        }
     }
-}
