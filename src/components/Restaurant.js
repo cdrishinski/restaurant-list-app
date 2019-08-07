257d5c4376d64af28a29d62c0ad3e00d41586edf
cdrishinski <cdrishinski@gmail.com>
2019-08-07 16:25:32 -0600

    adding search capability

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -6,41 +6,51 @@ export default class Restaurant extends Component {
     constructor(props){
         super(props)
         this.state= {
-          restDetail: 
-              {
-                  name: '',
-                  times: ''
-              }
+            search: "",
+            searchResults: []
     }
 }
 
 
-
+filterRestaurants (){
+    let localArr = RestaurantList.filter(currVal => {
+       return currVal.name.indexOf(this.state.search) != -1; 
+    })
+    console.log(localArr)
+}
    
 
     render() {
+        console.log(this.state.search)
         return (
             <div>
                 <h1>Restaurant page!</h1>
-            
-                { RestaurantList.map( (restDetail, index) =>{
-                    return(
-                        <ul>
-                            <li>{ restDetail.name }
-                            </li>
-                                <ul>
-                                    <li> { restDetail.times[0] }</li>
-                                    <li> { restDetail.times[1] }</li>
-                                    <li> { restDetail.times[2] }</li>
-                                </ul>
-                        </ul>
-                        
-                    )
-                    
-                })}
-               
 
+                <input type="text" placeholder="Search Restaurants" onChange={(text) => this.setState({search: text.target.value})} value={this.state.search} />
+
+            
+                <ul>
+                    { this.filterRestaurants() }
+                {/* { RestaurantList.filter( (restDetail, index) =>{ 
+                //         (
+                //             <div>
+                //             <li>{ restDetail.name }
+                //             </li>
+                //                     <ul>
+                //                         <li> { restDetail.times[0] }</li>
+                //                         <li> { restDetail.times[1] }</li>
+                //                         <li> { restDetail.times[2] }</li>
+                //                     </ul>
+                //         </div>
+                // )
+                  
+                        
+                    {/* })} */}
+                    </ul>
+               
+                    
              </div>
             )
         }
     }
+    
